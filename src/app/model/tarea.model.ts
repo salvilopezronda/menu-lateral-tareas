cce982de683dcff2bcdaf8f503e3a1ec0827e1ee
salvador <salvilopezronda@gmail.com>
2023-05-15 12:45:56 +0200

    [02] prueba

--- src/app/model/tarea.model.ts
+++ src/app/model/tarea.model.ts
@@ -4,16 +4,19 @@ export class Tarea {
   descripcion: string;
   estado: number;
   tipologia: number;
+  fecha: string
   constructor(data?: any) {
     this.id = (data && data.id) || null;
     this.titulo = (data && data.titulo) || null;
     this.descripcion = (data && data.descripcion) || null;
     this.estado = (data && data.estado) || null;
     this.tipologia = (data && data.tipologia) || null;
+    this.fecha = (data && data.fecha) ||null;
   }
   public toService() {
     const obj: any = new Object();
     obj.id = this.id;
+    obj.fecha =this.fecha;
     obj.titulo = this.titulo;
     obj.descripcion = this.descripcion;
     obj.estado = this.estado;
